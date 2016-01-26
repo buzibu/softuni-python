a4ded1aa1987cc26a5a9d84be46d9fb94763aeb7
vladislav <vzaimov@yahoo.com>
2016-01-27 00:59:24 +0200

    add new files

--- time_sales_with_hour.py
+++ time_sales_with_hour.py
@@ -1,29 +1,26 @@
 from datetime import datetime
 
 sales = {
-'1': 0,
-'2': 0,
-'3': 0,
-'4': 0,
-'5': 0,
-'6': 0,
-'0': 0
+
 }
 
 with open("sales.csv") as f:
     for line in f:
         sales_data = line.split(',')
         datetime_value = datetime.strptime(sales_data[0], '%Y-%m-%d %H:%M:%S')
-        sales[datetime_value.strftime('%d %H')]+=float(sales_data[1])
+        dict_key = datetime_value.strftime('%w %H')
+        if dict_key in sales:
+            sales[dict_key] += float(sales_data[1])
+        else:
+            sales[dict_key] = float(sales_data[1])
 
 maximum = 0.0
 maxday = ''
 
 for ind in sales:
-    print("Day {} : {} sales".format(ind,sales[ind]))
+    print("Time {} : {} sales".format(ind,sales[ind]))
     if sales[ind] > maximum:
         maximum = sales[ind]
         maxday = ind
 
-print('Maximum sales {} on day {}'.format(maximum,maxday))
-    
+print('Maximum sales {} on Time {}'.format(maximum,maxday))
